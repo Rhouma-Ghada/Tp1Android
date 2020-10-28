24d65c6d3a27cd3f42faa8bdc70aa84c56f20111
ghada rhouma <ghada.rhouma.123@gmail.com>
2020-10-28 08:11:09 +0100

    création du projet et initialisation du git

---
+++ app/src/main/java/com/example/tp1android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tp1android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.util.Log ;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("qest1", "3");
+        Log.d("qest2", "1");
+        Log.d("qest3", "2");
+        Log.d("qest4", "3");
+        Log.d("ghada", "23");
+    }
+}
